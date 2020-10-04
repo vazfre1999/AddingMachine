d2e13c30bdc657ac006cf8545474009e1924dc92
Luis Vazquez <vazfre1999@gmail.com>
2020-10-04 14:49:00 -0700

    Calculator

---
+++ CSE360/src/cse360assignment02/Calculator.java
@@ -0,0 +1,22 @@
+package cse360assignment02;
+import java.util.Scanner;
+
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		//scanner object
+		Scanner in = new Scanner(System.in);
+		//object for addingMachine
+		AddingMachine myCalculator = new AddingMachine();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
